ec5f213bd4039d46f15423f85a3d4d968fc322a0
NSANZIMFURA <marcnsanzimfura20@gmail.com>
2023-12-08 03:48:06 -0800

    we are pushing this codes

--- arguments.c
+++ arguments.c
@@ -1,38 +1,38 @@
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define MAX_ARGS 10
 #define MAX_INPUT 1024
 
 int main() {
-    char *args[MAX_ARGS + 1]; // Additional space for the NULL terminator
     char input[MAX_INPUT];
     size_t len = MAX_INPUT;
-    ssize_t read;
+    ssize_t bytes_read; // Renamed 'read' to 'bytes_read'
 
     // Read the command line input
     write(STDOUT_FILENO, "Enter command: ", 15);
-    read = getline(&input, &len, stdin);
-    if (read == -1) {
+    bytes_read = read(STDIN_FILENO, input, MAX_INPUT);
+    if (bytes_read == -1) {
         perror("Error reading input");
         exit(EXIT_FAILURE);
     }
+    input[bytes_read - 1] = '\0'; // Removing newline character
 
     // Tokenize the input into arguments
     int argc = 0;
-    char *token = strtok(input, " \n");
+    char *args[MAX_ARGS + 1]; // Additional space for the NULL terminator
+    char *token = strtok(input, " ");
     while (token != NULL && argc < MAX_ARGS) {
         args[argc++] = token;
-        token = strtok(NULL, " \n");
+        token = strtok(NULL, " ");
     }
     args[argc] = NULL; // Set the last element to NULL for execve
 
     // Execute the command
     if (argc > 0) {
-        if (execve(args[0], args, NULL) == -1) {
+        if (execvp(args[0], args) == -1) {
             perror("Execution error");
             exit(EXIT_FAILURE);
         }
